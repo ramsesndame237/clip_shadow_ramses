9911406d811c3164552941b3ad078dac8b65d5a0
Ramses ndame <64875585+ramsesndame237@users.noreply.github.com>
2022-01-31 11:12:31 +0100

    plugin flutter clipshadow

---
+++ examples/lib/clip_shadow.dart
@@ -0,0 +1,68 @@
+library clip_shadow;
+
+import 'package:flutter/widgets.dart';
+
+class _ClipShadowPainter extends CustomPainter {
+  /// If non-null, determines which clip to use.
+  final CustomClipper<Path> clipper;
+
+  /// A list of shadows cast by this box behind the box.
+  final List<BoxShadow> clipShadow;
+
+  _ClipShadowPainter( {
+    this.clipper, this.clipShadow,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    clipShadow.forEach((BoxShadow shadow) {
+      var paint = shadow.toPaint();
+      var spreadSize = Size(size.width + shadow.spreadRadius * 2, size.height + shadow.spreadRadius * 2);
+      print(spreadSize);
+      var clipPath = clipper.getClip(spreadSize).shift(Offset(
+        shadow.offset.dx - shadow.spreadRadius,
+        shadow.offset.dy - shadow.spreadRadius
+      ));
+      canvas.drawPath(clipPath, paint);
+//      canvas.drawShadow(clipper.getClip(size), shadow.color, shadow.spreadRadius, true);
+    });
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class ClipShadow extends StatelessWidget {
+  /// A list of shadows cast by this box behind the box.
+  final List<BoxShadow> boxShadow;
+
+  /// If non-null, determines which clip to use.
+  final CustomClipper<Path> clipper;
+
+  /// The [Widget] below this widget in the tree.
+  final Widget child;
+
+  ClipShadow({
+     this.boxShadow,
+     this.clipper,
+     this.child
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      painter: _ClipShadowPainter(
+          clipShadow: boxShadow,
+          clipper: clipper
+      ),
+      child: ClipPath(
+        clipper: clipper,
+        child: child,
+      ),
+    );
+  }
+}
+
+
---
+++ test/clip_shadow_ramses_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:clip_shadow_ramses/clip_shadow_ramses.dart';
+
+void main() {
+
+}
